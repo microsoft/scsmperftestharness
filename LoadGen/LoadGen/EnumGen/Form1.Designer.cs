0c868bf79098ae7f431326e2acc4fc73f2992bb3
radtravis_cp <SND\radtravis_cp>
2013-10-10 05:59:36 +0000

    Created EnumGen project to generate a large volume of enumerations.

---
+++ LoadGen/LoadGen/EnumGen/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace EnumGen
+{
+    partial class frmEnumGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSCSMServerName = new System.Windows.Forms.Label();
+            this.lblNewMPName = new System.Windows.Forms.Label();
+            this.lblBaseEnumName = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNumberOfEnumsToCreatePerLevel = new System.Windows.Forms.Label();
+            this.txtSCSMServerName = new System.Windows.Forms.TextBox();
+            this.txtNewMPName = new System.Windows.Forms.TextBox();
+            this.txtBaseEnumName = new System.Windows.Forms.TextBox();
+            this.nudNumberOfLevelsToCreate = new System.Windows.Forms.NumericUpDown();
+            this.nudNumberOfEnumsToCreatePerLevel = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblFolderToWriteMPFileTo = new System.Windows.Forms.Label();
+            this.txtFolderToWriteFileTo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfLevelsToCreate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfEnumsToCreatePerLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSCSMServerName
+            // 
+            this.lblSCSMServerName.AutoSize = true;
+            this.lblSCSMServerName.Location = new System.Drawing.Point(13, 13);
+            this.lblSCSMServerName.Name = "lblSCSMServerName";
+            this.lblSCSMServerName.Size = new System.Drawing.Size(101, 13);
+            this.lblSCSMServerName.TabIndex = 0;
+            this.lblSCSMServerName.Text = "SCSM server name:";
+            // 
+            // lblNewMPName
+            // 
+            this.lblNewMPName.AutoSize = true;
+            this.lblNewMPName.Location = new System.Drawing.Point(13, 63);
+            this.lblNewMPName.Name = "lblNewMPName";
+            this.lblNewMPName.Size = new System.Drawing.Size(80, 13);
+            this.lblNewMPName.TabIndex = 1;
+            this.lblNewMPName.Text = "New MP name:";
+            // 
+            // lblBaseEnumName
+            // 
+            this.lblBaseEnumName.AutoSize = true;
+            this.lblBaseEnumName.Location = new System.Drawing.Point(13, 113);
+            this.lblBaseEnumName.Name = "lblBaseEnumName";
+            this.lblBaseEnumName.Size = new System.Drawing.Size(92, 13);
+            this.lblBaseEnumName.TabIndex = 2;
+            this.lblBaseEnumName.Text = "Base enum name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Number of levels to create:";
+            // 
+            // lblNumberOfEnumsToCreatePerLevel
+            // 
+            this.lblNumberOfEnumsToCreatePerLevel.AutoSize = true;
+            this.lblNumberOfEnumsToCreatePerLevel.Location = new System.Drawing.Point(13, 213);
+            this.lblNumberOfEnumsToCreatePerLevel.Name = "lblNumberOfEnumsToCreatePerLevel";
+            this.lblNumberOfEnumsToCreatePerLevel.Size = new System.Drawing.Size(181, 13);
+            this.lblNumberOfEnumsToCreatePerLevel.TabIndex = 4;
+            this.lblNumberOfEnumsToCreatePerLevel.Text = "Number of enums to create per level:";
+            // 
+            // txtSCSMServerName
+            // 
+            this.txtSCSMServerName.Location = new System.Drawing.Point(16, 30);
+            this.txtSCSMServerName.Name = "txtSCSMServerName";
+            this.txtSCSMServerName.Size = new System.Drawing.Size(178, 20);
+            this.txtSCSMServerName.TabIndex = 5;
+            this.txtSCSMServerName.Text = "localhost";
+            // 
+            // txtNewMPName
+            // 
+            this.txtNewMPName.Location = new System.Drawing.Point(16, 80);
+            this.txtNewMPName.Name = "txtNewMPName";
+            this.txtNewMPName.Size = new System.Drawing.Size(178, 20);
+            this.txtNewMPName.TabIndex = 6;
+            this.txtNewMPName.Text = "NewMP";
+            // 
+            // txtBaseEnumName
+            // 
+            this.txtBaseEnumName.Location = new System.Drawing.Point(16, 130);
+            this.txtBaseEnumName.Name = "txtBaseEnumName";
+            this.txtBaseEnumName.Size = new System.Drawing.Size(178, 20);
+            this.txtBaseEnumName.TabIndex = 7;
+            this.txtBaseEnumName.Text = "IncidentClassificationEnum";
+            // 
+            // nudNumberOfLevelsToCreate
+            // 
+            this.nudNumberOfLevelsToCreate.Location = new System.Drawing.Point(16, 180);
+            this.nudNumberOfLevelsToCreate.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfLevelsToCreate.Name = "nudNumberOfLevelsToCreate";
+            this.nudNumberOfLevelsToCreate.Size = new System.Drawing.Size(120, 20);
+            this.nudNumberOfLevelsToCreate.TabIndex = 8;
+            this.nudNumberOfLevelsToCreate.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // nudNumberOfEnumsToCreatePerLevel
+            // 
+            this.nudNumberOfEnumsToCreatePerLevel.Location = new System.Drawing.Point(16, 230);
+            this.nudNumberOfEnumsToCreatePerLevel.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNumberOfEnumsToCreatePerLevel.Name = "nudNumberOfEnumsToCreatePerLevel";
+            this.nudNumberOfEnumsToCreatePerLevel.Size = new System.Drawing.Size(120, 20);
+            this.nudNumberOfEnumsToCreatePerLevel.TabIndex = 9;
+            this.nudNumberOfEnumsToCreatePerLevel.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(414, 304);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblFolderToWriteMPFileTo
+            // 
+            this.lblFolderToWriteMPFileTo.AutoSize = true;
+            this.lblFolderToWriteMPFileTo.Location = new System.Drawing.Point(16, 257);
+            this.lblFolderToWriteMPFileTo.Name = "lblFolderToWriteMPFileTo";
+            this.lblFolderToWriteMPFileTo.Size = new System.Drawing.Size(141, 13);
+            this.lblFolderToWriteMPFileTo.TabIndex = 11;
+            this.lblFolderToWriteMPFileTo.Text = "Folder to write the MP file to:";
+            // 
+            // txtFolderToWriteFileTo
+            // 
+            this.txtFolderToWriteFileTo.Location = new System.Drawing.Point(16, 274);
+            this.txtFolderToWriteFileTo.Name = "txtFolderToWriteFileTo";
+            this.txtFolderToWriteFileTo.Size = new System.Drawing.Size(371, 20);
+            this.txtFolderToWriteFileTo.TabIndex = 12;
+            this.txtFolderToWriteFileTo.Text = "C:\\";
+            // 
+            // frmEnumGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 339);
+            this.Controls.Add(this.txtFolderToWriteFileTo);
+            this.Controls.Add(this.lblFolderToWriteMPFileTo);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.nudNumberOfEnumsToCreatePerLevel);
+            this.Controls.Add(this.nudNumberOfLevelsToCreate);
+            this.Controls.Add(this.txtBaseEnumName);
+            this.Controls.Add(this.txtNewMPName);
+            this.Controls.Add(this.txtSCSMServerName);
+            this.Controls.Add(this.lblNumberOfEnumsToCreatePerLevel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblBaseEnumName);
+            this.Controls.Add(this.lblNewMPName);
+            this.Controls.Add(this.lblSCSMServerName);
+            this.Name = "frmEnumGen";
+            this.Text = "EnumGen";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfLevelsToCreate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumberOfEnumsToCreatePerLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSCSMServerName;
+        private System.Windows.Forms.Label lblNewMPName;
+        private System.Windows.Forms.Label lblBaseEnumName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNumberOfEnumsToCreatePerLevel;
+        private System.Windows.Forms.TextBox txtSCSMServerName;
+        private System.Windows.Forms.TextBox txtNewMPName;
+        private System.Windows.Forms.TextBox txtBaseEnumName;
+        private System.Windows.Forms.NumericUpDown nudNumberOfLevelsToCreate;
+        private System.Windows.Forms.NumericUpDown nudNumberOfEnumsToCreatePerLevel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblFolderToWriteMPFileTo;
+        private System.Windows.Forms.TextBox txtFolderToWriteFileTo;
+    }
+}
+
---
+++ LoadGen/LoadGen/EnumGen/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.EnterpriseManagement;
+using Microsoft.EnterpriseManagement.Common;
+using Microsoft.EnterpriseManagement.Configuration;
+using Microsoft.EnterpriseManagement.Configuration.IO;
+using Microsoft.EnterpriseManagement.Packaging;
+using Microsoft.EnterpriseManagement.Helper;
+
+
+namespace EnumGen
+{
+    public partial class frmEnumGen : Form
+    {
+        static int iNumberOfLevels;
+        static int iNumberOfEnumsPerLevel;
+        int iNumberOfLevelsCreated = 0;
+        int iNumberOfEnumsCreated = 0;
+           
+        public frmEnumGen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            iNumberOfLevelsCreated = 0;
+            iNumberOfEnumsCreated = 0;
+            EnterpriseManagementGroup emg = new EnterpriseManagementGroup(txtSCSMServerName.Text);
+            ManagementPackEnumeration mpeBase = Helper.GetEnumerationByName(txtBaseEnumName.Text, emg);
+            ManagementPack mpNew = new ManagementPack(txtNewMPName.Text, txtNewMPName.Text, new Version("1.0.0.0"), emg);
+            iNumberOfLevels = (int)nudNumberOfLevelsToCreate.Value;
+            iNumberOfEnumsPerLevel = (int)nudNumberOfEnumsToCreatePerLevel.Value;
+            CreateEnumerationsAtALevel("Enum", ref mpeBase, ref mpNew, iNumberOfEnumsPerLevel);
+            DialogResult dr = MessageBox.Show(String.Format("Number of enums to be created: {0}", iNumberOfEnumsCreated.ToString()), "Write to MP XML file?", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                mpNew.AcceptChanges();
+                ManagementPackXmlWriter mpxw = new ManagementPackXmlWriter(txtFolderToWriteFileTo.Text);
+                mpxw.WriteManagementPack(mpNew);
+            }
+        }
+
+        private void CreateEnumerationsAtALevel(string strBaseEnumName, ref ManagementPackEnumeration mpeBase, ref ManagementPack mpNew, int iNumberOfEnums)
+        {
+            int i = 0;
+            do
+            {
+                //Create an enumeration
+                ManagementPackEnumeration mpe = CreateEnumeration(String.Format("{0}.{1}", strBaseEnumName, (i+1).ToString()), ref mpeBase, ref mpNew);
+                //If you haven't already reached the bottom of the levels, create the children of this enumeration
+                if (iNumberOfLevelsCreated < iNumberOfLevels-1)
+                {
+                    iNumberOfLevelsCreated++;
+                    CreateEnumerationsAtALevel(mpe.Name, ref mpe, ref mpNew, iNumberOfEnumsPerLevel);
+                }
+
+                i++;
+                //If we are all done creating enums at this level then go back up a level.
+                if (i == iNumberOfEnums)
+
+                {
+                    iNumberOfLevelsCreated--;
+                }
+
+            } while (i < iNumberOfEnums);
+            
+        }
+
+        private ManagementPackEnumeration CreateEnumeration(string strName, ref ManagementPackEnumeration mpeBase, ref ManagementPack mpNew)
+        {
+            ManagementPackEnumeration mpeNew = new ManagementPackEnumeration(mpNew, strName, ManagementPackAccessibility.Public);
+            mpeNew.Parent = mpeBase;
+            mpeNew.DisplayName = String.Format("Test Value {0}",strName);
+            iNumberOfEnumsCreated++;
+            return mpeNew;
+        }
+    }
+}
